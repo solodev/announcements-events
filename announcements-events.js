da517e2046c310cdc84781a7ef1f0c58fe649ad2
SeanDanielsDigital <34247055+SeanDanielsDigital@users.noreply.github.com>
2018-05-03 11:10:42 -0400

    upload files

---
+++ announcements-events.js
@@ -0,0 +1,21 @@
+	$('.announcement-slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		prevArrow: $('.prev-arrow-announcement'),
+		nextArrow: $('.next-arrow-announcement')
+	});
+
+	$('.events').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		prevArrow: $('.prev-arrow-events'),
+		nextArrow: $('.next-arrow-events')
+	});
